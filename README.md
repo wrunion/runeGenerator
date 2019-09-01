a62326daf24b75c2af68388fdc49e766f3c1fcd3
Winter Runion <winterrunion@gmail.com>
2019-09-01 13:12:31 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Elder FUTHARK Rune Generator
+
+#### A simple app to give you a randomly generated rune from the Elder Futhark, along with a poem about its meaning.
+
+_by Winter Runion_
---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var runes = ["Fehu", "Uruz", "Thurisaz", "Ansuz", "Raidho", "Kaunan", "Gebo", "Wunjo", "Hagalaz", "Naudhiz", "Isaz", "Jera", "Eihwaz", "Perdhro", "Elhaz", "Sowulo", "Teiwaz", "Berkana", "Ehwaz", "Mannaz", "Laguz", "Inguz", "Dagaz", "Othala"]; //array of the elder Futhark runes
+
+// var getRandomInt = function(min,max) {
+//   return Math.floor(Math.random() * (max-min) + min);
+// };
+
+var getRuneNumber = function() {
+  return Math.floor(Math.random() * 23);
+};
+
+
+
+
+//When to use let vs var???
